281a9a6b1bae4dfd4707db78562312c0d6a32402
Mar Roca <m.rocacugat@student.maastrichtuniversity.nl>
2025-12-23 11:58:29 +0100

    BIG PUMPKIN

--- pumpkinz.py
+++ pumpkinz.py
@@ -1,14 +1,19 @@
 from functions import *
 
 desiredWaterLevel = 0.4
+pumpkinSize = 0
 goToBeginning()
 
 while True:
-    for i in range(get_world_size()):
-        if not can_harvest():
-            plantGrassland(Entities.Pumpkin)
-        
-        if get_water() < desiredWaterLevel:
-            use_item(Items.Water)
-        move(North)
-    move(East)
+	for i in range(get_world_size()):
+		if not plantGrassland(Entities.Pumpkin):
+			pumpkinSize += 1
+				
+		if get_water() < desiredWaterLevel:
+			use_item(Items.Water)
+		
+		move(North)
+	move(East)
+	if pumpkinSize == 64:
+		harvest()
+		pumpkinSize = 0
