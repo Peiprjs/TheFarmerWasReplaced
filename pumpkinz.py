1312e9902da559c471ac5933a3d44b851f94c482
Mar Roca <m.rocacugat@student.maastrichtuniversity.nl>
2025-12-23 12:04:41 +0100

    Hopefully optimized now

--- pumpkinz.py
+++ pumpkinz.py
@@ -6,14 +6,15 @@ goToBeginning()
 
 while True:
 	for i in range(get_world_size()):
+		if pumpkinSize == get_world_size()**2:
+			harvest()
+			pumpkinSize = 0
 		if not plantGrassland(Entities.Pumpkin):
 			pumpkinSize += 1
 				
 		if get_water() < desiredWaterLevel:
 			use_item(Items.Water)
-		if pumpkinSize == get_world_size()**2:
-			harvest()
-			pumpkinSize = 0
+		
 	
 		move(North)
 	move(East)
