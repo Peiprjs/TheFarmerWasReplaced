5def303ca7204b69591ba2d33cbb40da81204132
Mar Roca <m.rocacugat@student.maastrichtuniversity.nl>
2025-12-23 21:38:06 +0100

    Trying apples

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ desiredWaterLevel = 0.4
 goToBeginning()
 	
 while True:
-	do_a_flip()
+	for i in range(get_world_size()):
+		for e in range(get_world_size()):
+			harvest()
+			plant(Entities.Cactus)
+			move(North)
+			if get_water() < desiredWaterLevel:
+				use_item(Items.Water)
+		move(East)
+
