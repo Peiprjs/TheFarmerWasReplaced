958b80163ecca4b71ddb1df213145c166db297a4
Mar Roca <m.rocacugat@student.maastrichtuniversity.nl>
2025-12-23 11:46:38 +0100

    beeg pumpking

--- pumpkinz.py
+++ pumpkinz.py
@@ -3,20 +3,12 @@ from functions import *
 desiredWaterLevel = 0.4
 goToBeginning()
 
-while get_pos_x() <= 4:
-    while get_pos_y() <= 4:
-        if get_entity_type() == Entities.Dead_Pumpkin:
-            plantGrassland(Entities.Pumpkin)
-        elif can_harvest():
-            harvest()
-            plantGrassland(Entities.Pumpkin)
-        else:
+while True:
+    for i in range(get_world_size()):
+        if not can_harvest():
             plantGrassland(Entities.Pumpkin)
         
         if get_water() < desiredWaterLevel:
             use_item(Items.Water)
-            
         move(North)
-        
     move(East)
-	
